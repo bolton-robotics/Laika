cfe810e7717366727df851ee801fc515be7313f0
bolton-robotics <apoole20@live.co.uk>
2023-05-01 12:08:17 +0100

    Completed Simulator

--- src/laika_bringup/launch/laika.sim.launch.py
+++ src/laika_bringup/launch/laika.sim.launch.py
@@ -1,98 +1,137 @@
+#####################################################################
+# Filename: laika.sim.launch.py                                  #
+# Date: 1 May 2023                                               #
+#                                                                   #
+# Description: Launches Gazebo, RViz2, Teleop Twist Keyboard as     #
+#              well as the Gazebo 'basic' world and 'odom' RViz2    #
+#              configuration.  Robot State Publisher is launched.   #
+#              Gazebo provides skid steer (diff drive) config, as   #
+#              well as Joint States, IMU, Lidar and Camera feeds.   #
+#                                                                   #
+#####################################################################
+
 import os
 
 from ament_index_python.packages import get_package_share_directory
 
 
 from launch import LaunchDescription
-from launch.actions import IncludeLaunchDescription
+from launch.actions import ExecuteProcess, IncludeLaunchDescription, RegisterEventHandler
+from launch.event_handlers import OnProcessExit
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 
 from launch_ros.actions import Node
 
+import xacro
 
 
 def generate_launch_description():
-
-
-    # Include the robot_state_publisher launch file, provided by our own package. Force sim time to be enabled
-    # !!! MAKE SURE YOU SET THE PACKAGE NAME CORRECTLY !!!
-
-    package_name='laika_description' #<--- CHANGE ME
-
-    rsp = IncludeLaunchDescription(
+    
+    robot_world = 'laika.basic.world'
+    robot_world_name = os.path.join(
+        get_package_share_directory('laika_description'),
+        robot_world)
+    
+# Specify the file paths for Rviz2 and XACRO arguments
+    rviz_file_name = 'laika.gazebo.rviz'
+    rviz = os.path.join(
+        get_package_share_directory('laika_description'),
+        rviz_file_name)
+    with open(rviz, 'r') as infp:
+        robot_desc = infp.read()
+    
+    gazebo = IncludeLaunchDescription(
                 PythonLaunchDescriptionSource([os.path.join(
-                    get_package_share_directory(package_name),'launch','rsp.launch.py'
-                )]), launch_arguments={'use_sim_time': 'true', 'use_ros2_control': 'true'}.items()
+                    get_package_share_directory('gazebo_ros'), 'launch'), '/gazebo.launch.py']),
+                    launch_arguments={'world': robot_world_name}.items()
+             )
+
+    laika_description_path = os.path.join(
+        get_package_share_directory('laika_description'))
+    
+    xacro_file = os.path.join(laika_description_path,'sim.urdf.xacro')
+
+    doc = xacro.process_file(xacro_file)
+    params = doc.toprettyxml(indent='  ')
+
+    node_robot_state_publisher = Node(
+        package='robot_state_publisher',
+        executable='robot_state_publisher',
+        output='screen',
+        parameters=[{'use_sim_time': True, 'robot_description': params}],
+        arguments=[xacro_file]
     )
 
-    joystick = IncludeLaunchDescription(
-                PythonLaunchDescriptionSource([os.path.join(
-                    get_package_share_directory(package_name),'launch','joystick.launch.py'
-                )]), launch_arguments={'use_sim_time': 'true'}.items()
+    spawn_entity = Node(package='gazebo_ros', executable='spawn_entity.py',
+                        arguments=['-topic', 'robot_description',
+                                   '-entity', 'diffbot'],
+                        output='screen')
+
+    load_joint_state_controller = ExecuteProcess(
+        cmd=['ros2', 'control', 'load_controller', '--set-state', 'active',
+             'joint_state_broadcaster'],
+        output='screen'
     )
 
-    twist_mux_params = os.path.join(get_package_share_directory(package_name),'config','twist_mux.yaml')
-    twist_mux = Node(
+    load_diff_drive_base_controller = ExecuteProcess(
+        cmd=['ros2', 'control', 'load_controller', '--set-state', 'active',
+             'diff_drive_controller'],
+        output='screen'
+    )
+
+  # Joystick and Twist_Mux
+    joy_params = os.path.join(get_package_share_directory('laika_description'),'joystick.yaml')
+
+    start_joystick_cmd = Node(
+            package='joy',
+            executable='joy_node',
+            parameters=[joy_params, {'use_sim_time': True}],
+         )
+
+    twist_mux_params = os.path.join(get_package_share_directory('laika_description'),'twist_mux.yaml')
+
+    start_twist_mux_cmd = Node(
             package="twist_mux",
             executable="twist_mux",
             parameters=[twist_mux_params, {'use_sim_time': True}],
-            remappings=[('/cmd_vel_out','/diff_cont/cmd_vel_unstamped')]
+            remappings=[('/cmd_vel_out','/diff_drive_controller/cmd_vel_unstamped')]
         )
 
-    gazebo_params_file = os.path.join(get_package_share_directory(package_name),'config','gazebo_params.yaml')
+    start_teleop_cmd = Node(
+            package='teleop_twist_joy',
+            executable='teleop_node',
+            name='teleop_node',
+            parameters=[joy_params, {'use_sim_time': True}],
+            remappings=[('/cmd_vel','/cmd_vel_joy')]
+         )
+ 
+   # Launch RViz
 
-    # Include the Gazebo launch file, provided by the gazebo_ros package
-    gazebo = IncludeLaunchDescription(
-                PythonLaunchDescriptionSource([os.path.join(
-                    get_package_share_directory('gazebo_ros'), 'launch', 'gazebo.launch.py')]),
-                    launch_arguments={'extra_gazebo_args': '--ros-args --params-file ' + gazebo_params_file}.items()
-             )
+    start_rviz_cmd = Node(
+    package='rviz2',
+    executable='rviz2',
+    name='rviz2',
+    output='screen',
+    arguments=['-d', rviz])
 
-    # Run the spawner node from the gazebo_ros package. The entity name doesn't really matter if you only have a single robot.
-    spawn_entity = Node(package='gazebo_ros', executable='spawn_entity.py',
-                        arguments=['-topic', 'robot_description',
-                                   '-entity', 'my_bot'],
-                        output='screen')
-
-
-    diff_drive_spawner = Node(
-        package="controller_manager",
-        executable="spawner.py",
-        arguments=["diff_cont"],
-    )
-
-    joint_broad_spawner = Node(
-        package="controller_manager",
-        executable="spawner.py",
-        arguments=["joint_broad"],
-    )
-
-
-    # Code for delaying a node (I haven't tested how effective it is)
-    # 
-    # First add the below lines to imports
-    # from launch.actions import RegisterEventHandler
-    # from launch.event_handlers import OnProcessExit
-    #
-    # Then add the following below the current diff_drive_spawner
-    # delayed_diff_drive_spawner = RegisterEventHandler(
-    #     event_handler=OnProcessExit(
-    #         target_action=spawn_entity,
-    #         on_exit=[diff_drive_spawner],
-    #     )
-    # )
-    #
-    # Replace the diff_drive_spawner in the final return with delayed_diff_drive_spawner
-
-
-
-    # Launch them all!
     return LaunchDescription([
-        rsp,
-        joystick,
-        twist_mux,
+        RegisterEventHandler(
+            event_handler=OnProcessExit(
+               target_action=spawn_entity,
+                on_exit=[load_joint_state_controller],
+            )
+        ),
+        RegisterEventHandler(
+            event_handler=OnProcessExit(
+                target_action=load_joint_state_controller,
+                on_exit=[load_diff_drive_base_controller],
+            )
+        ),
         gazebo,
+        node_robot_state_publisher,
         spawn_entity,
-        diff_drive_spawner,
-        joint_broad_spawner
+        start_joystick_cmd,
+        start_twist_mux_cmd,
+        start_teleop_cmd,
+        start_rviz_cmd
     ])
